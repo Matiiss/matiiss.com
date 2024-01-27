232a2ce4598d26991189c1e0b6aaacc98eaa5b0e
Matiiss <83066658+Matiiss@users.noreply.github.com>
2024-01-27 06:47:08 +0200

    migrating to flask

---
+++ flask_app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+# from flask_assets import Environment, Bundle
+
+app = Flask(__name__)
+
+# assets = Environment(app)
+# assets.url = app.static_url_path
+# scss = Bundle("sass/style.scss", filters="sass", output="style.css")
+# assets.register("style", scss)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
