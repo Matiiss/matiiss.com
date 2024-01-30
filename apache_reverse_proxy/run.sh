2b41f21c6275718d0f187541b44e7f553690b40e
Matiiss <83066658+Matiiss@users.noreply.github.com>
2024-01-30 18:58:41 +0200

    SSL certificate config

---
+++ apache_reverse_proxy/run.sh
@@ -0,0 +1,9 @@
+acme.sh/acme.sh --install -m my@example.com
+acme.sh/acme.sh --issue --standalone -d matiiss.com -d www.matiiss.com
+acme.sh/acme.sh --installcert  -d matiiss.com -d www.matiiss.com \
+                --certpath /usr/local/ssl/matiiss.com.pem \
+                --keypath /usr/local/ssl/matiiss.com.key \
+                --capath /usr/local/ssl/matiiss.com.pem \
+                --reloadcmd "systemctl restart apache2"
+
+httpd-foreground
