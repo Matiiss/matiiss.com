063dae24e7ea8a42a32ec98895423348fb443935
Matiiss <83066658+Matiiss@users.noreply.github.com>
2024-01-30 20:22:20 +0200

    change root's home dir

--- apache_reverse_proxy/run.sh
+++ apache_reverse_proxy/run.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-cd ./acme.sh
-./acme.sh --install -m my@example.com
-./acme.sh --issue -d matiiss.com -d www.matiiss.com
-./acme.sh --installcert  -d matiiss.com -d www.matiiss.com \
+acme.sh/acme.sh --install -m my@example.com
+acme.sh/acme.sh --issue -d matiiss.com -d www.matiiss.com
+acme.sh/acme.sh --installcert  -d matiiss.com -d www.matiiss.com \
                 --certpath /usr/local/ssl/matiiss.com.pem \
                 --keypath /usr/local/ssl/matiiss.com.key \
                 --capath /usr/local/ssl/matiiss.com.pem \
                 --reloadcmd "systemctl restart apache2"
-cd ..
 
 httpd-foreground
