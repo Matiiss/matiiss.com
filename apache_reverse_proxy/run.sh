ea2f1b915d4b3dd4ca1045e5d49425e0fb054a76
Matiiss <83066658+Matiiss@users.noreply.github.com>
2024-01-30 20:38:34 +0200

    format run script, clone only depth 1

--- apache_reverse_proxy/run.sh
+++ apache_reverse_proxy/run.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
-acme.sh/acme.sh --install --standalone -m my@example.com \
+acme.sh/acme.sh --install \
+                --standalone \
+                -m my@example.com \
                 --home /usr/local
-acme.sh/acme.sh --issue -d matiiss.com -d www.matiiss.com
-acme.sh/acme.sh --installcert  -d matiiss.com -d www.matiiss.com \
+                
+acme.sh/acme.sh --issue \
+                -d matiiss.com \
+                -d www.matiiss.com
+
+acme.sh/acme.sh --installcert \
+                -d matiiss.com \
+                -d www.matiiss.com \
                 --certpath /usr/local/ssl/matiiss.com.pem \
                 --keypath /usr/local/ssl/matiiss.com.key \
                 --capath /usr/local/ssl/matiiss.com.pem \
