61223308b026a6bea24adde273f90eb114e2d1b6
Matiiss <83066658+Matiiss@users.noreply.github.com>
2024-01-30 20:41:07 +0200

    cd into cloned repo

--- apache_reverse_proxy/run.sh
+++ apache_reverse_proxy/run.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
 
-acme.sh/acme.sh --install \
+cd ./acme.sh
+./acme.sh --install \
                 --standalone \
                 -m my@example.com \
                 --home /usr/local/.acme.sh
 
-acme.sh/acme.sh --issue \
+./acme.sh --issue \
                 -d matiiss.com \
                 -d www.matiiss.com
 
-acme.sh/acme.sh --installcert \
+./acme.sh --installcert \
                 -d matiiss.com \
                 -d www.matiiss.com \
                 --certpath /usr/local/ssl/matiiss.com.pem \
                 --keypath /usr/local/ssl/matiiss.com.key \
                 --capath /usr/local/ssl/matiiss.com.pem \
                 --reloadcmd "systemctl restart apache2"
+cd ..
 
 httpd-foreground
