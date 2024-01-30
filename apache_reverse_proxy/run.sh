1110b49aacc217e4bcf891d358f8b6a721f0af2e
Matiiss <83066658+Matiiss@users.noreply.github.com>
2024-01-30 19:47:20 +0200

    remove debug echos

--- apache_reverse_proxy/run.sh
+++ apache_reverse_proxy/run.sh
@@ -1,13 +1,9 @@
-echo "hi"
 acme.sh/acme.sh --install -m my@example.com
-echo "hi 2"
 acme.sh/acme.sh --issue --standalone -d matiiss.com -d www.matiiss.com
-echo "hi 3"
 acme.sh/acme.sh --installcert  -d matiiss.com -d www.matiiss.com \
                 --certpath /usr/local/ssl/matiiss.com.pem \
                 --keypath /usr/local/ssl/matiiss.com.key \
                 --capath /usr/local/ssl/matiiss.com.pem \
                 --reloadcmd "systemctl restart apache2"
-echo "hi 4"
 
 httpd-foreground
