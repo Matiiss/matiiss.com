9d3f5c12347a9723306e767210e42366d7a8a656
Matiiss <83066658+Matiiss@users.noreply.github.com>
2024-01-31 04:30:21 +0200

    disable cert stuff for now

--- apache_reverse_proxy/run.sh
+++ apache_reverse_proxy/run.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 
-cd ./acme.sh
-./acme.sh --install \
-          -m matiiss@matiiss.com \
-          --home /usr/local/.acme.sh
+# cd ./acme.sh
+# ./acme.sh --install \
+#           -m matiiss@matiiss.com \
+#           --home /usr/local/.acme.sh
 
-cd /usr/local/.acme.sh
+# cd /usr/local/.acme.sh
 
-./acme.sh --issue \
-          -d matiiss.com \
-          --standalone \
-          --keylength ec-256 \
-          --server letsencrypt \
-          --debug
+# ./acme.sh --issue \
+#           -d matiiss.com \
+#           --standalone \
+#           --keylength ec-256 \
+#           --server letsencrypt \
+#           --debug
 
-./acme.sh --installcert \
-          -d matiiss.com \
-          --cert-file /usr/local/apache2/htdocs/matiiss.com.pem \
-          --key-file /usr/local/apache2/htdocs/matiiss.com.key \
-          --fullchain-file /usr/local/apache2/htdocs/fullchain.pem \
-          --reloadcmd "systemctl restart apache2" \
-          --debug
+# ./acme.sh --installcert \
+#           -d matiiss.com \
+#           --cert-file /usr/local/apache2/htdocs/matiiss.com.pem \
+#           --key-file /usr/local/apache2/htdocs/matiiss.com.key \
+#           --fullchain-file /usr/local/apache2/htdocs/fullchain.pem \
+#           --reloadcmd "systemctl restart apache2" \
+#           --debug
 
-cd /usr/local
+# cd /usr/local
 
 httpd-foreground
