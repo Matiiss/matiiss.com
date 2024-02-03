1e7fdb75460e81a6fad21002c4b0ac36d3669b0d
Matiiss <83066658+Matiiss@users.noreply.github.com>
2024-02-03 04:52:30 +0200

    disable flask cache

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,6 +3,9 @@ from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
 
+# let nginx cache the static files
+app.config.update(SEND_FILE_MAX_AGE_DEFAULT=0)
+
 assets = Environment(app)
 assets.url = app.static_url_path
 scss = Bundle("sass/style.scss", filters="pyscss", output="css/style.css")
